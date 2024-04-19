1ed8272b172b801d296e511abcfe56d30e0b5dc5
Gustavo 紫 <52300498+gustavocorreiaof@users.noreply.github.com>
2024-04-18 23:00:51 -0300

    Primeiro componente

---
+++ React/fundamentos/src/index.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import First from './Components/Basics/FirstComponent'
+
+ReactDOM.render(
+    <div>
+        <First></First>
+    </div>,
+    document.getElementById('root')
+)

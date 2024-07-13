c4ed175ee8224e5b1ff588760e9d3b81f3a98e62
Gustavo 紫 <52300498+gustavocorreiaof@users.noreply.github.com>
2024-07-13 13:24:39 -0300

    33

---
+++ FirstAPI/FirstAPI/AppDbContext/AppDbContext.cs
@@ -0,0 +1,12 @@
+﻿using FirstAPI.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace FirstAPI.AppDbContext;
+
+public class AppDbContext : DbContext
+{
+    public AppDbContext(DbContextOptions<AppDbContext> options) : base(options){}
+    DbSet<Category> Categories { get; set; }
+    DbSet<Product> Products { get; set; }
+
+}
---
+++ FirstAPI/FirstAPI/Models/Product.cs
@@ -0,0 +1,14 @@
+﻿namespace FirstAPI.Models;
+
+public class Product
+{
+    public int ProductId { get; set; }
+    public string? Name { get; set; }
+    public string? Description { get; set; }
+    public string? ImageUrl { get; set; }
+    public bool Stock { get; set;}
+    public DateTime CreationDate { get; set; }
+    public decimal Value{ get; set; }
+    public int CategoryId{ get; set; }
+    public Category? Category { get; set; }
+}

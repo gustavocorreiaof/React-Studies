fecb806ca8a713c8528f10c01b21131d877018f9
Gustavo 紫 <52300498+gustavocorreiaof@users.noreply.github.com>
2024-09-02 23:40:44 -0300

    Resto do desafio

---
+++ React/fundamentos/src/data/produtos.js
@@ -0,0 +1,7 @@
+export default [
+    { Id: 1, nome: 'Mouse', preco: 140 },
+    { Id: 2, nome: 'Teclado', nota: 244 },
+    { Id: 3, nome: 'Mesa', nota: 400 },
+    { Id: 4, nome: 'Whey', nota: 100 },
+    { Id: 5, nome: 'Sexo', nota: 0 }
+]

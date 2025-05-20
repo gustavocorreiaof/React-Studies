809d928ada18ce7031728ff60add94028f348bb7
Gustavo 紫 <52300498+gustavocorreiaof@users.noreply.github.com>
2025-05-20 20:22:48 -0300

    remove unecessary files

--- primeirocurso/src/App.js
+++ primeirocurso/src/App.js
@@ -1,25 +1,10 @@
-import logo from './logo.svg';
-import './App.css';
 import Banner from './components/Banner/Banner';
 
 function App() {
   return (
     <div className="App">
       <Banner></Banner>
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Wellcome to React!
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Murinas?Amigao?
-        </a>
-      </header>
+      
     </div>
   );
 }

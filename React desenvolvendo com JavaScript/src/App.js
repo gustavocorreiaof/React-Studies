dcf6bf1b23736027d2f2277379a06fe7d871bdfc
Gustavo 紫 <52300498+gustavocorreiaof@users.noreply.github.com>
2025-06-07 02:01:51 -0300

    end of first course

--- React desenvolvendo com JavaScript/src/App.js
+++ React desenvolvendo com JavaScript/src/App.js
@@ -1,70 +1,68 @@
 import { useState } from 'react';
-import Banner from './components/Banner/Banner';
-import Form from './components/Form';
-import TextField from './components/TextField';
-import Time from './components/Team';
+import Banner from './componentes/Banner';
+import Formulario from './componentes/Formulario';
+import Time from './componentes/Time';
 
 function App() {
-  
-const times = [
-  {
-    nome: 'Programação',
-    corPrimaria: '#57C278',
-    corSecundaria: '#D9F7E9'
-  },
-  {
-    nome: 'Front-end',
-    corPrimaria: '#82CFFA',
-    corSecundaria: '#E8F8FF'
-  },
-  {
-    nome: 'Data Science',
-    corPrimaria: '#A6D157',
-    corSecundaria: '#F0E8E2'
-  },
-  {
-    nome: 'Devops',
-    corPrimaria: '#E06B69',
-    corSecundaria: '#FDE7E8'
-  },
-  {
-    nome: 'Ux e Design',
-    corPrimaria: '#DB6EBF',
-    corSecundaria: '#FAE9F5'
-  },
-  {
-    nome: 'Mobile',
-    corPrimaria: '#FFBA05',
-    corSecundaria: '#FFF5D9'
-  },
-  {
-    nome: 'Inovação e Gestão',
-    corPrimaria: '#FF8A29',
-    corSecundaria: '#FFEEDF'
-  }
-]
+
+  const times = [
+    {
+      nome: 'Programação',
+      corPrimaria: '#57C278',
+      corSecundaria: '#D9F7E9'
+    },
+    {
+      nome: 'Front-End',
+      corPrimaria: '#82CFFA',
+      corSecundaria: '#E8F8FF'
+    },
+    {
+      nome: 'Data Science',
+      corPrimaria: '#A6D157',
+      corSecundaria: '#F0F8E2'
+    },
+    {
+      nome: 'Devops',
+      corPrimaria: '#E06B69',
+      corSecundaria: '#FDE7E8'
+    },
+    {
+      nome: 'UX e Design',
+      corPrimaria: '#DB6EBF',
+      corSecundaria: '#FAE9F5'
+    },
+    {
+      nome: 'Mobile',
+      corPrimaria: '#FFBA05',
+      corSecundaria: '#FFF5D9'
+    },
+    {
+      nome: 'Inovação e Gestão',
+      corPrimaria: '#FF8A29',
+      corSecundaria: '#FFEEDF'
+    }
+  ]
 
   const [colaboradores, setColaboradores] = useState([])
 
   const aoNovoColaboradorAdicionado = (colaborador) => {
-    console.log(colaborador)
+    debugger
     setColaboradores([...colaboradores, colaborador])
-    console.log(colaboradores)
   }
-  
+
   return (
     <div className="App">
-      <Banner></Banner>
-      <Form times={times.map(time => time.nome)} aoColaboradorCadastrado={colaborador => aoNovoColaboradorAdicionado(colaborador)}></Form>   
-      
-      {times.map(time => 
-      <Time 
+      <Banner />
+      <Formulario times={times.map(time => time.nome)} aoColaboradorCadastrado={colaborador => aoNovoColaboradorAdicionado(colaborador)}/>
+
+      {times.map(time => <Time 
         key={time.nome} 
-        Name={time.nome} 
+        nome={time.nome} 
         corPrimaria={time.corPrimaria} 
-        corSecundaria={time.corSecundaria}
-        colaboradores={colaboradores.filter(colaborador => colaborador.time === time.nome)}>
-      </Time>)}      
+        corSecundaria={time.corSecundaria} 
+        colaboradores={colaboradores.filter(colaborador => colaborador.time === time.nome)}
+      />)}   
+
     </div>
   );
 }
